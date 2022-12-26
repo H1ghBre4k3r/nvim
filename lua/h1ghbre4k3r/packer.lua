536d3767bedf46494f3ee8b68f6e46de4020b440
H1ghBre4k3r <h1ghbre4k3r@dev.bre4k3r.de>
2022-12-26 17:58:28 +0100

    Switch from gitsigns to gitgutter

--- lua/h1ghbre4k3r/packer.lua
+++ lua/h1ghbre4k3r/packer.lua
@@ -40,10 +40,7 @@ return require('packer').startup({ function(use)
 
     use { 'akinsho/bufferline.nvim', tag = "v3.*", requires = 'nvim-tree/nvim-web-devicons' }
 
-    use {
-        'lewis6991/gitsigns.nvim',
-        -- tag = 'release', -- To use the latest release (do not use this if you run Neovim nightly or dev builds!)
-    }
+    use "airblade/vim-gitgutter"
 
     use {
         "tpope/vim-fugitive"
