7319f39974ee0ef7d7c583711c62ee22efef4450
H1ghBre4k3r <h1ghbre4k3r@dev.bre4k3r.de>
2022-12-26 17:53:56 +0100

    Add neotree configuration

--- lua/h1ghbre4k3r/packer.lua
+++ lua/h1ghbre4k3r/packer.lua
@@ -86,6 +86,19 @@ return require('packer').startup({ function(use)
     }
 
     use "onsails/lspkind.nvim"
+
+    use {
+        "nvim-neo-tree/neo-tree.nvim",
+        branch = "v2.x",
+        requires = {
+            "nvim-lua/plenary.nvim",
+            "nvim-tree/nvim-web-devicons", -- not strictly required, but recommended
+            "MunifTanjim/nui.nvim",
+        },
+        config = function()
+            vim.cmd([[ let g:neo_tree_remove_legacy_commands = 1 ]])
+        end
+    }
 end,
     config = {
         display = {
