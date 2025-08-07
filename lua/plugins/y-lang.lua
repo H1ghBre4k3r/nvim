6709783948d68b39103a0c5223ba1b06472c5dcf
H1ghBre4k3r <h1ghbre4k3r@dev.bre4k3r.de>
2025-08-07 13:50:08 +0200

    Update nvim config

--- lua/plugins/y-lang.lua
+++ lua/plugins/y-lang.lua
@@ -1,6 +1,6 @@
 return {
-  -- "H1ghBre4k3r/y-lang.nvim",
-  dir = "/Users/louis/git/y-lang.nvim",
+  "H1ghBre4k3r/y-lang.nvim",
+  -- dir = "/Users/louis/git/y-lang.nvim",
   init = function()
     require("y-lang").setup()
   end,
